a534e6c4bf1714b2f6bacdded174f4ea7564abfb
gabriel <metalmadnes98@gmail.com>
2022-04-19 22:18:17 -0500

    first commit

---
+++ test/spiderman.test.js
@@ -0,0 +1,14 @@
+// prueba erronea
+describe("Test Suite Dummy Description", () => {
+        test('Case 1 Dummy', () => {
+            const resultOfSomething = 1 + 2
+            expect(resultOfSomething).toBe(18);
+        });
+    })
+    // prueba correcta
+describe("Test Suite Dummy Description", () => {
+    test('Case 1 Dummy', () => {
+        const resultOfSomething = 1 + 2
+        expect(resultOfSomething).toBe(3);
+    });
+})
